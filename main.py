7f2d82e95227a1f8d08fe4c41fa0328926cdbb82
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2024-11-01 12:45:28 +0300

    Parameterizing values, second attempt

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ from time import *
 
 scene = canvas(width=1000, height=800, background=color.gray(0.3))
 
-floor       = box(pos=vector(0,-10,0), color=color.white, size=vector(20,.3,20))
-ceiling     = box(pos=vector(0,10,0),  color=color.white, size=vector(20,.3,20))
-right_wall  = box(pos=vector(10,0,0),  color=color.white, size=vector(.2,20,20))
-left_wall   = box(pos=vector(-10,0,0), color=color.white, size=vector(.2,20,20))
-back_wall   = box(pos=vector(0,0,-10), color=color.white, size=vector(20,20,.3))
+mRadius = 1
+wallThickness = .3
+roomWidth, roomDepth, roomHeight = 20,20,20
 
-marble      = sphere(color=color.red, radius=3)
+floor       = box(pos=vector(0,-roomHeight/2,0), color=color.white, size=vector(roomWidth,wallThickness,roomDepth))
+ceiling     = box(pos=vector(0,roomHeight/2,0),  color=color.white, size=vector(roomWidth,wallThickness,roomDepth))
+right_wall  = box(pos=vector(roomWidth/2,0,0),  color=color.white, size=vector(wallThickness,roomHeight,roomDepth))
+left_wall   = box(pos=vector(-roomWidth/2,0,0), color=color.white, size=vector(wallThickness,roomHeight,roomDepth))
+back_wall   = box(pos=vector(0,0,-roomDepth/2), color=color.white, size=vector(roomWidth,roomHeight,wallThickness))
+
+marble      = sphere(color=color.red, radius=mRadius)
 
 changeX = 0.1
 xPos = 0

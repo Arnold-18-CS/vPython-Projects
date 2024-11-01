29cfc79fe9b05ec39ad84c26ea8a3201f416303e
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2024-11-01 08:34:49 +0300

    Changing name of fle to main, used to run scenario created

--- main.py
+++ main.py
@@ -4,10 +4,8 @@
 from vpython import *
 from time import *
 
-ball = sphere()
-sleep(5)
-ball.color = color.red
-sleep(5)
-ball.color = color.blue
+# Making a tube
+tube = cylinder(radius=2)
+
 while True:
     pass

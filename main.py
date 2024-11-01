efbc2e3f432ef5d58d68d875eea1dc652b97aab9
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2024-11-01 09:33:01 +0300

    Parameterizing the dimensions using size attribute, background changes

--- main.py
+++ main.py
@@ -4,13 +4,13 @@
 from vpython import *
 from time import *
 
-scene = canvas(width=1000, height=800, background=color.gray(0.5))
+scene = canvas(width=1000, height=800, background=color.gray(0.3))
 
-floor       = box(pos=vector(0,-10,0), color=color.white, length=20, width=20, height=.3)
-ceiling     = box(pos=vector(0,10,0), color=color.white, length=20, width=20, height=.3)
-right_wall  = box(pos=vector(10,0,0), color=color.white, length=.2, width=20, height=20)
-left_wall   = box(pos=vector(-10,0,0), color=color.white, length=.2, width=20, height=20)
-back_wall   = box(pos=vector(0,0,-10), color=color.white, length=20, width=.3, height=20)
+floor       = box(pos=vector(0,-10,0), color=color.white, size=vector(20,.3,20))
+ceiling     = box(pos=vector(0,10,0),  color=color.white, size=vector(20,.3,20))
+right_wall  = box(pos=vector(10,0,0),  color=color.white, size=vector(.2,20,20))
+left_wall   = box(pos=vector(-10,0,0), color=color.white, size=vector(.2,20,20))
+back_wall   = box(pos=vector(0,0,-10), color=color.white, size=vector(20,20,.3))
 
 marble      = sphere(color=color.red, radius=3)
 
@@ -19,7 +19,7 @@ xPos = 0
 
 while True:
     rate(20) # To change the speed of animation
-    
+
     xPos = xPos + changeX
 
     if xPos > 10 or xPos < -10:

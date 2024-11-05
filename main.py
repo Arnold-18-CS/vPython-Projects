d84bd92d240ea2a427c3bf12f3cc3a85a1697105
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2024-11-05 19:47:32 +0300

    Basic thermometer with mercury that goes up and down

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ import numpy as np
 
 scene = canvas(width=1000, height=800, background=color.gray(0.3))
 
-piston = cylinder(radius=1, length=3, color=color.red, opacity=0.5)
+# Creating a thermometer
+bulb = sphere(radius=1.25, color=color.white, opacity=.4)
+tube = cylinder(radius=0.6, length=5, color=color.white, opacity=.4)
+mercuryInBulb = sphere(radius=1, color=color.red, opacity=1)
+mercuryCol = cylinder(radius=0.5, length=4.5, color=color.red, opacity=1)
 
 while True:
-    for o in np.linspace(0,1,100):
-        rate(50)
-        piston.opacity = o
-    for o in np.linspace(1,0,100):
-        rate(50)
-        piston.opacity = o
+        for temp in np.linspace(1,5,100):
+                rate(20)
+                mercuryCol.length = temp
+        for temp in np.linspace(5,1,100):
+                rate(20)
+                mercuryCol.length = temp

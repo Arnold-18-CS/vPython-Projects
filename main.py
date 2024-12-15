dcae30294bdadd96b7ac7d5c5485046d2057ed7d
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2024-12-15 14:38:34 +0300

    Base code changes

--- main.py
+++ main.py
@@ -7,16 +7,52 @@ import numpy as np
 
 scene = canvas(width=1000, height=800, background=color.gray(0.3))
 
-# Creating a thermometer
-bulb = sphere(radius=1.25, color=color.white, opacity=.4)
-tube = cylinder(radius=0.6, length=5, color=color.white, opacity=.4)
-mercuryInBulb = sphere(radius=1, color=color.red, opacity=1)
-mercuryCol = cylinder(radius=0.5, length=4.5, color=color.red, opacity=1)
+mRadius = 1
+wallThickness = .3
+roomWidth, roomDepth, roomHeight = 20,20,20
+
+floor       = box(pos=vector(0,-roomHeight/2,0), color=color.white, size=vector(roomWidth,wallThickness,roomDepth))
+ceiling     = box(pos=vector(0,roomHeight/2,0),  color=color.white, size=vector(roomWidth,wallThickness,roomDepth))
+right_wall  = box(pos=vector(roomWidth/2,0,0),  color=color.white, size=vector(wallThickness,roomHeight,roomDepth))
+left_wall   = box(pos=vector(-roomWidth/2,0,0), color=color.white, size=vector(wallThickness,roomHeight,roomDepth))
+back_wall   = box(pos=vector(0,0,-roomDepth/2), color=color.white, size=vector(roomWidth,roomHeight,wallThickness))
+
+xPos, yPos, zPos = 0, 0, 0
+marble      = sphere(pos=vector(xPos, yPos, zPos), color=color.red, radius=mRadius)
+
+changeX, changeY, changeZ = .1, .1, .1
 
 while True:
-        for temp in np.linspace(1,5,100):
-                rate(20)
-                mercuryCol.length = temp
-        for temp in np.linspace(5,1,100):
-                rate(20)
-                mercuryCol.length = temp
+    rate(30) # To change the speed of animation
+   
+    xPos += changeX
+    yPos += changeY
+    zPos += changeZ
+
+    xRMEdge = xPos + mRadius
+    xLMEdge = xPos - mRadius
+    yTMEdge = yPos + mRadius
+    yBMEdge = yPos - mRadius
+    zBMEdge = zPos + mRadius
+    zFMEdge = zPos - mRadius
+
+    RWEdge = roomWidth / 2 - wallThickness / 2
+    LWEdge = -roomWidth / 2 + wallThickness / 2
+    CEdge = roomHeight / 2 - wallThickness / 2
+    FlEdge = -roomHeight / 2 + wallThickness / 2
+    BEdge = roomDepth / 2 + wallThickness / 2
+    FrEdge = - roomDepth / 2 + wallThickness / 2
+
+    if xRMEdge > RWEdge or xLMEdge < LWEdge:
+            changeX *= -1
+            marble.color = color.white
+
+    if yTMEdge > CEdge or yBMEdge < FlEdge:
+            changeY *= -1
+            marble.color = color.black
+
+    if zBMEdge > BEdge or zFMEdge < FrEdge:
+            changeZ *= -1
+            marble.color = color.blue 
+
+    marble.pos = vector(xPos,yPos,zPos)
